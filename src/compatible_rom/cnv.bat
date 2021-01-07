c8daa4a59c481953e8ac5cab03fdd0701b450168
windy6001 <windy-mail@zeus.eonet.ne.jp>
2021-01-07 15:37:04 +0900

    秋川様の互換ROMを内蔵をとりやめました。

--- src/compatible_rom/cnv.bat
+++ src/compatible_rom/cnv.bat
@@ -1,28 +1,21 @@
-set DIR=basic60-v07
-..\..\tools\bin2c %DIR%\BASICROM.60 >basicrom60.h
-rem ..\..\tools\bin2c %DIR%\BASICROM.62 >basicrom62.h
+
+set DIR=basic60-v072
+..\..\tools\bin2c %DIR%\c_BASICROM.60n >basicrom60.h
 set DIR=
 
-..\..\tools\bin2c CGROM60\CGROM60.60 >cgrom60.h
-
-set DIR=basic66-v022
-..\..\tools\bin2c %DIR%\BASICROM.66 >basicrom66.h
-..\..\tools\bin2c %DIR%\VOICEROM.66 >voicerom66.h
-
-..\..\tools\bin2c font66\KANJIROM.66 >kanjirom66.h
-..\..\tools\bin2c font66\CGROM60.66  >cgrom6066.h
-..\..\tools\bin2c font66\CGROM66.66  >cgrom6666.h
+..\..\tools\bin2c CGROM60\c_CGROM60.60n >cgrom60.h
 
 
-rem copy %DIR%\VOICEROM.66 %DIR%\VOICEROM.62
-rem copy %DIR%\KANJIROM.66 %DIR%\KANJIROM.62
-rem copy %DIR%\CGROM60.66  %DIR%\CGROM60.62
-rem copy %DIR%\CGROM66.66  %DIR%\CGROM60m.62
-rem ..\..\tools\bin2c %DIR%\CGROM60.62  >cgrom6062.h
-rem ..\..\tools\bin2c %DIR%\CGROM60m.62  >cgrom60m62.h
-rem ..\..\tools\bin2c %DIR%\VOICEROM.62 >voicerom62.h
-rem ..\..\tools\bin2c %DIR%\KANJIROM.62 >kanjirom62.h
+set DIR=basic66-v031
+..\..\tools\bin2c %DIR%\c_BASICROM.66n >basicrom66.h
+..\..\tools\bin2c %DIR%\c_VOICEROM.66n >voicerom66.h
 
-..\..\tools\bin2c %DIR%\EXKANJI.ROM >exkanjirom.h
+..\..\tools\bin2c font66\c_KANJIROM.66n >kanjirom66.h
+..\..\tools\bin2c font66\c_CGROM60.66n  >cgrom6066.h
+..\..\tools\bin2c font66\c_CGROM66.66n  >cgrom6666.h
+
+..\..\tools\bin2c font66\c_EXKANJI.ROMn >exkanjirom.h
+
 
 set DIR=
+pause
